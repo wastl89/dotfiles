779ec007b6e083033dac475ed1820a62c76ceb11
Sebastian Ratajczak <dermitdemblub@gmail.com>
2018-10-16 10:08:39 +0200

    First Commit

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ln -s ~/.dotfiles/i3 ~/.config/i3/config
+ln -s ~/.dotfiles/gtk-2 ~/.gtkrc-2.0
+ln -s ~/.dotfiles/gtk-3 ~/.config/gtk-3.0/settings.ini
+ln -s ~/.dotfiles/Xdefaults ~/.Xdefaults
